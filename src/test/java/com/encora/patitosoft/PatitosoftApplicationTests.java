3051fb3d9b3f2314974defc710e978af38098dd2
Oswaldo <bigosvaap@gmail.com>
2022-04-06 09:31:11 -0500

    Initial commit

---
+++ src/test/java/com/encora/patitosoft/PatitosoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.patitosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitosoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
